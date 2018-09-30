9f0741b3be5db212a7cb9a7dff81d326f7550151
Yura <ybeliaev@gmail.com>
2018-09-30 10:40:54 +0300

    New Math methods

---
+++ 05-new_Math_methods.js
@@ -0,0 +1,30 @@
+Math.sign() returns the sign of a number:
+
+> Math.sign(-8)
+-1
+> Math.sign(0)
+0
+> Math.sign(3)
+1
+
+Math.trunc() removes the decimal fraction of a number:
+
+> Math.trunc(3.1)
+3
+> Math.trunc(3.9)
+3
+> Math.trunc(-3.1)
+-3
+> Math.trunc(-3.9)
+-3
+
+Math.log10() computes the logarithm to base 10:
+
+> Math.log10(100)
+2
+
+Math.hypot() Computes the square root of the sum of the squares of its arguments (Pythagoras’ theorem):
+
+> Math.hypot(3, 4)
+5    
+
